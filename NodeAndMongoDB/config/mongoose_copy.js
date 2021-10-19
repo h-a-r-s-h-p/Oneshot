23123c0b30f0ff814bac3fede6ba6b827de99e99
h-a-r-s-h-p <111801015@smail.iitpkd.ac.in>
2021-10-19 11:30:26 +0530

    finally

---
+++ NodeAndMongoDB/config/mongoose_copy.js
@@ -0,0 +1,30 @@
+// require the lbrary
+const mongoose = require("mongoose");
+
+const connection_url = "<PUT YOUR STRING here>";
+
+// connect to the database
+// mongoose.connect('mongodb://localhost/college_db2');
+
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// acquire the connection (to check if it was successful)
+const db = mongoose.connection;
+
+// if error
+db.on("error", console.error.bind(console, "error connecting to db"));
+
+// if up and running then print mssg
+db.once("open", function () {
+  console.log("Succesfully connected to db");
+});
+
+/*
+DATABASE AUTH ::
+username: XXXX
+password: XXXX
+*/
+
